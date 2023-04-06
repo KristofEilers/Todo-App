acb2470f6b0a71c8f561ddbda0c937937121e5b7
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-04-06 17:46:39 +0200

    added the list function and buttons in app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+// Selectors
+const taskInput = document.querySelector(".task-input");
+const taskBtn = document.querySelector(".task-btn");
+const taskList = document.querySelector(".task-list");
+
+// Event Listeners
+taskBtn.addEventListener("click", addTask);
+
+// Functions
+function addTask(event) {
+  event.preventDefault();
+  //Task-List
+  const taskContainer = document.createElement("div");
+  taskContainer.classList.add("task");
+  // Li
+  const newTask = document.createElement("li");
+  newTask.classList.add("task-item");
+  newTask.innerText = "hey";
+  taskContainer.appendChild(newTask);
+  //Check Mark Button
+  const checkButton = document.createElement("button");
+  checkButton.innerHTML = '<i class="fas fa-check"></i>';
+  checkButton.classList.add("check-btn");
+  taskContainer.appendChild(checkButton);
+  //Delete Button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete-btn");
+  taskContainer.appendChild(deleteButton);
+  // Append to List
+  taskList.appendChild(taskContainer);
+}
